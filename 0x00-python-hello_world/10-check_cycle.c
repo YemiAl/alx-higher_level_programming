b2fc9babfff28ffbd97c1b2763235385a16ef241
YemiAl <oluyemialabi@gmail.com>
2023-08-23 01:40:43 +0000

    Edited script 10

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,5 +1,13 @@
 #include "lists.h"
 
+/**
+ * check_cycle - a function that checks if a cycle exists
+ *
+ * @list: pointer to the head of the list
+ *
+ * Return: 0 if no cycle and 1 if a cycle exists
+ */
+
 int check_cycle(listint_t *list)
 {
 	listint_t *fast, *slow;
